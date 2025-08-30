bb41829a0f85fc7e5665feb4c78f381b1bc31412
pprasan <pprasan2@jh.edu>
2025-08-29 22:35:29 -0700

    W1M1 : myPage initial commit

---
+++ module_1/assignment/myPage/pages.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint("pages", __name__)
+
+@bp.route("/")
+def home():
+    return render_template("pages/home.html")
+
+@bp.route("/about")
+def about():
+    return render_template("pages/about.html")
+
+@bp.route("/projects")
+def projects():
+    return render_template("pages/projects.html")
+
+@bp.route("/contact")
+def contact():
+    return render_template("pages/contact.html")
