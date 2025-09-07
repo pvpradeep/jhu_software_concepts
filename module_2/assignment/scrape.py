4862cbb4063e7feca040b622ef083741176e7419
pprasan <pprasan2@jh.edu>
2025-09-07 00:23:49 -0700

    W2M2: gradCafe scrapping - first 200 records

--- module_2/assignment/scrape.py
+++ module_2/assignment/scrape.py
@@ -3,50 +3,24 @@ import urllib3
 from bs4 import BeautifulSoup
 import json
 import time
+from clean import clean_data, print_record
 
-urlBase = "https://www.thegradcafe.com"
+
 surveys = "https://www.thegradcafe.com/survey/index.php?page={}"
 http = urllib3.PoolManager()
+applicant_data_file = 'applicant_data.json'
+records = []
 
-def getValueOfField(soup, field_name):
-    field = soup.find(string=field_name)
-    if field:
-        value = field.find_next().get_text(strip=True)
-        if value:
-            return value
-    return "N/A"
+def isStartOfNewRecord(row):
+    # this seems to be the only way to identify start of new record
+    div = row.find('div', class_='tw-font-medium tw-text-gray-900 tw-text-sm') 
 
-
-def getRecordFromDetailPage(detail_url):
-    response = http.request('GET', detail_url)
-    if (response.status != 200):
-        print("Error: Unable to fetch {detail_url}, errno = {response.status}")
-        return None
-
-    page = urllib3.PoolManager().request('GET', detail_url)
-    soup = BeautifulSoup(page.data, 'html.parser')
-
-    fields = {'Institution', 'Program', 'Degree Type', 'Degree\'s Country of Origin', 'Decision', 'Notification', 'Notes'}
-    details = {}
-    for field in fields:
-        details[field] = getValueOfField(soup, field)
-    #print(details)
-    return details
-
-def getDetailUrlFromRow(row):
-    # Search for the <a> tag with href containing '/result/' anywhere inside the row:
-    detail_link = row.find('a', string='See More')
-    detail_url = None
-
-    if detail_link and detail_link.has_attr('href'):
-        relative_url = detail_link['href']
-        # Normalize URL if needed, e.g. ignore fragment identifiers or trailing spaces
-        relative_url = relative_url.split()[0]  # To handle any trailing content after href link
-        detail_url = urlBase + relative_url
-        #print("Detail URL found:", detail_url)
-    return detail_url
-    
-def scrape_page(page_number):
+    #there should be some text in the div
+    if div and div.get_text(strip=True):
+        return True
+    return False
+   
+def scrape_one_page(page_number):
     url = surveys.format(page_number)
     print("Searching ", url)
 
@@ -54,7 +28,9 @@ def scrape_page(page_number):
     if (response.status != 200):
         print("Error: Unable to fetch {url}, errno = {response.status}")
         return
-
+    
+    i = 0   
+    
     page = urllib3.PoolManager().request('GET', url)
     soup = BeautifulSoup(page.data, 'html.parser')
 
@@ -62,45 +38,49 @@ def scrape_page(page_number):
     table = soup.find('table')
 
     # Get all rows (tr elements) in the table
-    rows = table.find_all('tr')
+    rows = table.find_all('tr')[1:] # 1: Skip header row
+    
+    while i < len(rows):
+        row = rows[i]
+        commentRow = False
+        if (isStartOfNewRecord(row)):
+            record, commentRow = clean_data(rows, i)
+            if record:
+                #print_record(record)
+                records.append(record)
+            i += 2 # move to next record (2 rows ahead)
+            if commentRow:
+                i += 1
+        else:
+            i += 1 # move to next row        
 
-    # Get header row (th elements)
-    header_row = table.find('tr')
-    headers = [th.get_text(strip=True) for th in header_row.find_all(['th', 'td'])]
+def save_data(records, filename):
+    # Save records to a JSON file
+    with open(filename, 'a', encoding='utf-8') as f:
+        for record in records:
+            json_line = json.dumps(record, ensure_ascii=False)
+            f.write(json_line + '\n')
 
-    data_rows = table.find_all('tr')[1:]
-    i = 0
-    for row in data_rows:
-        #print("***************", row)
-        #print("***************")
-        detail_url = getDetailUrlFromRow(row)
-        summary_cols = row.find_all('td')
-        summary_data = [col.get_text(strip=True) or "N/A" for col in summary_cols]
-        if detail_url != None:
-            record = {                
-                "detail_url": detail_url,
-                #"summary": summary_data
-            }
-            record.update(getRecordFromDetailPage(detail_url) or {})
-            print(record)
-        #else:
-            #print("No detail URL found for row:", summary_data)
-        i += 1
-        if i > 3:
-            break
-        
+def scrape_page(max_records):
+    nPage = 1
+    nRecords = 0
+    
+    while nRecords < max_records:
+        scrape_one_page(nPage)
+        nPage += 1
+        nRecords += len(records)
+        time.sleep(1) # Be polite and avoid overwhelming the server
 
-for page_number in range(1, 1):  # Scrape first 5 pages
-    scrape_page(page_number)
+        #Append each page data to the file
+        print(f"Saving {len(records)} records to {applicant_data_file}, total records: {nRecords}, nPage: {nPage}")
+        if len(records) != 0:
+            save_data(records, applicant_data_file)
+            records.clear()
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    scrape_page(1)
+    scrape_page(200)  # Scrape up to 200 records
     end_time = time.time()
     print(f"Scraping completed in {end_time - start_time} seconds.")
 
-#page = urllib3.PoolManager().request('GET', url)
-#soup = BeautifulSoup(page.data, 'html.parser')
-
-#print(soup.title.string)
